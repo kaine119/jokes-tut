474bd4c4a8ba50b2bd598f8b1b4c51214086efe6
Mui Kai En <muikaien1@gmail.com>
2016-07-29 17:30:46 +0800

    Made it better
    its yuuuge

--- public/index.js
+++ public/index.js
@@ -1,10 +1,52 @@
-// $(document).ready(function(){
-	// var $setup = $("#setup");
-	// var $show = $("#showPunchline");
-	// var $punchline = $("#punchline");
+$(document).ready(function(){
+	var $setup = $("#setup");
+	var $show= $("#showPunchline");
+	var $punchline = $("#punchline");
+	var $container = $("#punchlineContainer")
+	var $score = $("#score");
+	var currentId = -1;
+	
+	var getJoke = function(){
+		$.get("./jokes", function(data){
+			$setup.html(data.setup);
+			$punchline.html(data.punchline);
+			$container.css("opacity", "0");
+			$score.html(data.score)
+			currentId = data.id;
+			
+		});
+	}
+	getJoke();
+	$("#getNew").click(function(){
+		getJoke();
+	})
 
-	$.get("/jokes", function(res){
-		console.log(res)
-		console.log("hi!")
+	$show.click(function(){
+		$container.css("opacity", "1")
 	});
-// });
+
+	$("#up").click(function(){
+		$.post({
+			url: "/upvote",
+			contentType: "application/json",
+			data: JSON.stringify({id: currentId}),
+			success: function(data){
+				$score.html(data.newScore);
+			}
+		});
+	});
+
+	$("#down").click(function(){
+		$.post({
+			url: "/downvote",
+			contentType: "application/json",
+			data: JSON.stringify({id: currentId}),
+			success: function(data){
+				$score.html(data.newScore);
+			}
+		});
+	});
+
+
+})
+
